5f108e18d7402c3428b3144fe4afc8a89ab30e20
BiG ADLER <95148142+BiG-ADLER@users.noreply.github.com>
2023-05-11 01:05:13 +0430

    added enter event

--- script.js
+++ script.js
@@ -8,37 +8,50 @@ let Day = $("#day");
 
 $(document).ready(function() {
     $(".calculate").click(function(){
-        if ((Year.val() && Month.val() && Day.val()) != (null || "")) {
-            (CalculateDate(Year.val(), Month.val(), Day.val()))
-        }
+        MouseClick()
+    });
 
-        if (Year.val() == (null || "")) {
-            Year.css({
-                'border': '1px solid red'
-            })
-            $('#year1').css({
-                'color': 'hsl(0, 100%, 67%)'
-            })
-        }
-        if (Month.val() == "") {
-            Month.css({
-                'border': '1px solid red'
-            })
-            $('#month1').css({
-                'color': 'hsl(0, 100%, 67%)'
-            })
-        }
-        if (Day.val() == "") {
-            Day.css({
-                'border': '1px solid red'
-            })
-            $('#day1').css({
-                'color': 'hsl(0, 100%, 67%)'
-            })
+    $("#body").keypress(function(e){
+        var key = e.which;
+        if (key == 13) {
+            MouseClick()
         }
     });
 });
 
+
+// jQuery Function Initial
+function MouseClick() {
+
+    if ((Year.val() && Month.val() && Day.val()) != (null || "")) {
+        (CalculateDate(Year.val(), Month.val(), Day.val()))
+    }
+
+    if (Year.val() == (null || "")) {
+        Year.css({
+            'border': '1px solid red'
+        })
+        $('#year1').css({
+            'color': 'hsl(0, 100%, 67%)'
+        })
+    }
+    if (Month.val() == "") {
+        Month.css({
+            'border': '1px solid red'
+        })
+        $('#month1').css({
+            'color': 'hsl(0, 100%, 67%)'
+        })
+    }
+    if (Day.val() == "") {
+        Day.css({
+            'border': '1px solid red'
+        })
+        $('#day1').css({
+            'color': 'hsl(0, 100%, 67%)'
+        })
+    }
+}
 
 // Calculate Age Function
 /**
@@ -51,7 +64,6 @@ function CalculateDate(year, month, day) {
     const CalculateMonth = 12 - month
     const CalculateDay = 31 - day
     if (CalculateYear < 0 ) {
-        console.log('ridi year')
         return false
     } else {
         if (CalculateMonth < 0) {
